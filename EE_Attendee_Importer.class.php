02df79934b389e465c76f37d712e18e9bc6ef07e
Mike Nelson <michael@eventespresso.com>
2019-03-12 12:15:49 -0700

    importing again

--- EE_Attendee_Importer.class.php
+++ EE_Attendee_Importer.class.php
@@ -159,6 +159,10 @@ Class  EE_Attendee_Importer extends EE_Addon {
             ],
             'EventEspresso\AttendeeImporter\core\domain\services\import\managers\ui\ImportCsvAttendeesUiManager' => [
                 'EventEspresso\core\services\loaders\Loader' => EE_Dependency_Map::load_from_cache
+            ],
+            'EventEspresso\AttendeeImporter\core\libraries\batch\JobHandlers\AttendeeImporterBatchJob' => [
+                'EventEspresso\AttendeeImporter\core\domain\services\import\csv\attendees\config\ImportCsvAttendeesConfig' => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\options\JsonWpOptionManager' => EE_Dependency_Map::load_from_cache,
             ]
         );
         foreach ($attendee_mover_dependencies as $class => $dependencies) {
--- core/libraries/batch/JobHandlers/AttendeeImporterBatchJob.php
+++ core/libraries/batch/JobHandlers/AttendeeImporterBatchJob.php
@@ -2,16 +2,22 @@
 
 namespace EventEspresso\AttendeeImporter\core\libraries\batch\JobHandlers;
 
-use EED_Attendee_Importer;
 use EventEspresso\AttendeeImporter\core\domain\services\commands\ImportCsvRowCommand;
+use EventEspresso\AttendeeImporter\core\domain\services\import\csv\attendees\config\ImportCsvAttendeesConfig;
+use EventEspresso\AttendeeImporter\core\services\import\extractors\ImportExtractorCsv;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidFilePathException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
 use EventEspresso\core\services\loaders\LoaderFactory;
+use EventEspresso\core\services\options\JsonWpOptionManager;
 use EventEspressoBatchRequest\Helpers\BatchRequestException;
 use EventEspressoBatchRequest\Helpers\JobParameters;
 use EventEspressoBatchRequest\Helpers\JobStepResponse;
 use EventEspressoBatchRequest\JobHandlerBaseClasses\JobHandler;
+use InvalidArgumentException;
 use LogicException;
 use RuntimeException;
-use SplFileObject;
+use EEH_File;
 
 /**
  * Class AttendeeImporterBatchJob
@@ -27,6 +33,26 @@ use SplFileObject;
 class AttendeeImporterBatchJob extends JobHandler
 {
 
+    /**
+     * @var ImportCsvAttendeesConfig
+     */
+    private $config;
+    /**
+     * @var JsonWpOptionManager
+     */
+    private $option_manager;
+
+    public function __construct(
+        ImportCsvAttendeesConfig $config,
+        JsonWpOptionManager $option_manager
+    )
+    {
+
+        $this->config = $config;
+        $this->option_manager = $option_manager;
+        $this->option_manager->populateFromDb($config);
+    }
+
 
     /**
      * Performs any necessary setup for starting the job. This is also a good
@@ -39,10 +65,12 @@ class AttendeeImporterBatchJob extends JobHandler
      */
     public function create_job(JobParameters $job_parameters)
     {
-        $config = EED_Attendee_Importer::instance()->getConfig();
-        $file = new SplFileObject($config->file, 'r');
+        $import_extractor = new ImportExtractorCsv();
+        $import_extractor->setSource($this->config->getFile());
+
         // Get the header row
-        if ($file->eof()) {
+        $csv_row = $import_extractor->getNextItem();
+        if (empty($csv_row)) {
             // The file's totally empty. That's whack.
             $job_parameters->set_status(JobParameters::status_error);
             return new JobStepResponse(
@@ -50,15 +78,12 @@ class AttendeeImporterBatchJob extends JobHandler
                 esc_html__('No comma-separated data was retrieved from the CSV file provided.', 'event_espresso')
             );
         }
-        $csv_row = $file->fgetcsv();
         $job_parameters->set_extra_data(
             [
                 'headers' => $csv_row
             ]
         );
-        // Now count the lines
-        $file->seek(PHP_INT_MAX);
-        $job_parameters->set_job_size($file->key() - 1);
+        $job_parameters->set_job_size($import_extractor->countItems());
         return new JobStepResponse(
             $job_parameters,
             esc_html__('Beginning import...', 'event_espresso')
@@ -70,35 +95,35 @@ class AttendeeImporterBatchJob extends JobHandler
      * @param JobParameters $job_parameters
      * @param int $batch_size
      * @return JobStepResponse
-     * @throws BatchRequestException
+     * @throws InvalidDataTypeException
+     * @throws InvalidFilePathException
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
      */
     public function continue_job(JobParameters $job_parameters, $batch_size = 50)
     {
-
-
+        $import_extractor = new ImportExtractorCsv();
+        $import_extractor->setSource($this->config->getFile());
 
         $command_bus = LoaderFactory::getLoader()->getShared('EventEspresso\core\services\commands\CommandBus');
         // grab the line from the file
-        $config = EED_Attendee_Importer::instance()->getConfig();
-        $file = new SplFileObject($config->file, 'r');
-        // Jump to the line we were at, plus one because we didn't count the header row
-        $file->seek($job_parameters->units_processed() + 1);
         $processed_this_batch = 0;
         $column_headers = $job_parameters->extra_datum('headers');
-        while (!$file->eof() && $processed_this_batch < $batch_size) {
-            $numeric_csv_row = $file->fgetcsv();
-            if(is_array($numeric_csv_row) && count($numeric_csv_row) === count($column_headers)) {
+        while ($csv_row = $import_extractor->getItemAt($job_parameters->units_processed() + 1 + $processed_this_batch)
+            && $processed_this_batch < $batch_size) {
+
+            if (is_array($csv_row) && count($csv_row) === count($column_headers)) {
                 $command_bus->execute(
                     new ImportCsvRowCommand(
                         array_combine(
                             $column_headers,
-                            $numeric_csv_row
+                            $csv_row
                         )
                     )
                 );
             }
             $processed_this_batch++;
-        } ;
+        };
         $job_parameters->mark_processed($processed_this_batch);
         if ($job_parameters->units_processed() >= $job_parameters->job_size()) {
             $job_parameters->set_status(JobParameters::status_complete);
@@ -121,10 +146,7 @@ class AttendeeImporterBatchJob extends JobHandler
      */
     public function cleanup_job(JobParameters $job_parameters)
     {
-        // TODO: Update ticket and datetime sold counts.
-        // But there could be a lot, so just do it for the ones actually affected.
-//        EEM_Ticket::instance()->update_tickets_sold();
-//        EEM_Datetime::instance()->update_sold();
+        EEH_File::delete(dirname($this->config->getFile()));
         return new JobStepResponse(
             $job_parameters,
             esc_html__('Summarizing import...', 'event_espresso')
