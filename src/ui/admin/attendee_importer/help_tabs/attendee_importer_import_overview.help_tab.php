2d772b5772d9226ced00044b4ef87335d423852f
Mike Nelson <michael@eventespresso.com>
2019-04-25 11:43:48 -0700

    make brents suggested tweaks and move all the info about the CSV file into the upload step help tab. I then realized the overview help tab could give a better overview too

--- src/ui/admin/attendee_importer/help_tabs/attendee_importer_import_overview.help_tab.php
+++ src/ui/admin/attendee_importer/help_tabs/attendee_importer_import_overview.help_tab.php
@@ -1,26 +1,26 @@
 <p><?php esc_html_e('Before Using the Event Espresso 4 Attendee Importer', 'event_espresso'); ?></p>
 <p>
     <strong><?php esc_html_e('Backup', 'event_espresso');?></strong>
-    <?php esc_html_e("Always make sure you have a complete database backup. If you import data improperly, or there is an error, this is the best way to undo the import.", 'event_espresso'); ?>
+    <?php esc_html_e("Always make sure you have a complete database backup before performing an import.", 'event_espresso'); ?>
 </p>
 <p>
     <strong><?php esc_html_e('Setup The Event in Event Espresso', 'event_espresso');?></strong>
-    <?php esc_html_e("You must first create the event and ticket you wish to import the attendees to. Currently, only importing to a single event's ticket is supported.", 'event_espresso'); ?>
+</p>
+<p>
+    <?php esc_html_e("You must first create the event and ticket you wish to import the attendees to. Currently, only importing to one event ticket at a time is supported.", 'event_espresso'); ?>
 </p>
 <p>
     <strong><?php esc_html_e('Prepare CSV File', 'event_espresso');?></strong>
-    <?php esc_html_e("Create your Comma-Separated-Value (CSV) file. The first row should be column headers, and each row subsequent represents a registration.", 'event_espresso'); ?>
 </p>
 <p>
-    <?php esc_html_e("You can give any name you want to the column headers. But there must be a column for:", 'event_espresso'); ?>
+    <?php esc_html_e('Please see the help tab about uploading the file for info on how to prepare the CSV file.', 'event_espresso'); ?>
 </p>
-<ul>
-    <li><?php esc_html_e('First Name', 'event_espresso');?></li>
-    <li><?php esc_html_e('Email', 'event_espresso');?></li>
-</ul>
-<p><?php esc_html_e('You can also add columns for any of the following:', 'event_espresso');?></p>
-<ul>
-    <li><?php esc_html_e('Any other system questions, like Last Name, Address, City, State, Country, Zip, or Phone. (Please note: for state you can use the state’s name, abbreviation, or database ID; for country you can use its name or ISO2 or ISO3 code)', 'event_espresso'); ?></li>
-    <li><?php esc_html_e('Any custom questions. The column header’s text does not need to match the custom question. For checkbox or multi-select questions, separate the different answer values with a pipe "|". Eg "', 'event_espresso'); ?></li>
-    <li><?php esc_html_e('The payment amount, if the registrations are for a paid ticket.', 'event_espresso'); ?></li>
-</ul>
+<p>
+    <strong><?php esc_html_e('Run the Importer', 'event_espresso'); ?></strong>
+</p>
+<p>
+    <?php esc_html_e('Complete the import configuration steps. You will have a chance to verify your configuration of the import before the data is actually imported. Before the verification step, use your browser’s back button if you like.', 'event_espresso'); ?>
+</p>
+<p>
+    <?php esc_html_e('If you import data improperly, or there is an error, the best way to undo the import is to restore to a backup, so please verify you have one..', 'event_espresso'); ?>
+</p>
--- src/ui/admin/attendee_importer/help_tabs/attendee_importer_import_upload.help_tab.php
+++ src/ui/admin/attendee_importer/help_tabs/attendee_importer_import_upload.help_tab.php
@@ -4,6 +4,24 @@
 <p>
     <?php esc_html_e('Choose the CSV file from your computer. It will be temporarily uploaded to the server and its rows will be imported.', 'event_espresso'); ?>
 </p>
+<p><strong><?php esc_html_e('About the CSV File Format', 'event_espresso'); ?></strong></p>
+<p>
+<?php esc_html_e("The first row should be column headers, and each subsequent row represents a registration.", 'event_espresso'); ?>
+</p>
+<p>
+    <?php esc_html_e("You can give any name you want to the column headers. But there must be a column for:", 'event_espresso'); ?>
+</p>
+<ul>
+    <li><?php esc_html_e('First Name', 'event_espresso');?></li>
+    <li><?php esc_html_e('Email', 'event_espresso');?></li>
+</ul>
+<p><?php esc_html_e('You can also add columns for any of the following:', 'event_espresso');?></p>
+<ul>
+    <li><?php esc_html_e('Any other system questions, like Last Name, Address, City, State, Country, Zip, or Phone.', 'event_espresso'); ?></li>
+    <li><?php esc_html_e('Please note: for state you can use the state’s name (e.g., "Alabama") or abbreviation (e.g., "AL"); for country you can use its name (e.g., "United States"), ISO2 code (e.g., "US"), or ISO3 code (e.g., "USA").', 'event_espresso'); ?></li>
+    <li><?php esc_html_e('Any custom questions. The column header’s text does not need to match the custom question. For checkbox or multi-select questions, separate the different answer values with a pipe "|". Eg "Breakfast|Lunch|Dinner"', 'event_espresso'); ?></li>
+    <li><?php esc_html_e('The payment amount, if the registrations are for a paid ticket.', 'event_espresso'); ?></li>
+</ul>
 <p>
     <a href="<?php echo EE_ATTENDEE_IMPORTER_URL;?>../samples/simple-with-custom-questions.csv">
     <?php esc_html_e('Click here to see a sample CSV file.', 'event_espresso');?>
