f3cf285fccfafb1926e13cfc019691f955f23604
Josh Feck <josh@nxnweb.com>
2019-05-06 17:22:21 -0400

    update i18n-ready strings that include Event Espresso branding to use a placeholder & helper method

--- src/domain/services/import/csv/attendees/templates/ee_attendee_importer_mapping_instructions.template.php
+++ src/domain/services/import/csv/attendees/templates/ee_attendee_importer_mapping_instructions.template.php
@@ -1,6 +1,15 @@
+<?php
+use EventEspresso\core\domain\Domain;
+?>
 <div class="ee-attendee-importer-mapping-instructions">
-    <p><?php esc_html_e('For each CSV column on the left, choose what Event Espresso data it will get mapped to during the import.', 'event_espresso');?></p>
-    <p><?php esc_html_e('The following Event Espresso data must be mapped to a column:', 'event_espresso');?></p>
+    <p><?php printf(
+    	esc_html__('For each CSV column on the left, choose what %s data it will get mapped to during the import.', 'event_espresso'),
+    	Domain::brandName()
+    );?></p>
+    <p><?php printf(
+    	esc_html__('The following %s data must be mapped to a column:', 'event_espresso'),
+    	Domain::brandName()
+    );?></p>
     <ul>
         <li><?php esc_html_e('First Name', 'event_espresso');?></li>
         <li><?php esc_html_e('Email Address', 'event_espresso');?></li>
