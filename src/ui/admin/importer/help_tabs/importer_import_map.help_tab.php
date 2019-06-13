645e7d486e20e7c4cfd1e884157dfe1d8f77a431
Mike Nelson <michael@eventespresso.com>
2019-06-13 10:46:29 -0700

    document the correct multi-value separator, the pipe character

--- src/ui/admin/importer/help_tabs/importer_import_map.help_tab.php
+++ src/ui/admin/importer/help_tabs/importer_import_map.help_tab.php
@@ -3,21 +3,44 @@ use EventEspresso\core\domain\Domain;
 ?>
 <p>
     <strong>
-        <?php printf(
+        <?php
+        printf(
+            // translators: %s Event Espresso
             esc_html__('Map the CSV File’s Columns to %s Data', 'event_espresso'),
             Domain::brandName()
         );?>
     </strong>
 </p>
 <p>
-    <?php printf(
+    <?php
+    printf(
+        // translators:  %s Event Espresso
         esc_html__(
+                // @codingStandardsIgnoreStart
             'On the left are the names of columns found in the CSV file you just uploaded. In each dropdown list are the system questions, custom questions, and other %s data that can be populated from the cell’s value.',
+            // @codingStandardsIgnoreEnd
             'event_espresso'
         ),
         Domain::brandName()
+    );
+    ?>
+</p>
+<p>
+    <?php
+    esc_html_e(
+            // @codingStandardsIgnoreStart
+            'Only the First Name and Email system questions must be mapped, all others are optional. You can also leave columns unmapped if you like.',
+            // @codingStandardsIgnoreEnd
+            'event_espresso'
     ); ?>
 </p>
 <p>
-    <?php esc_html_e('Only the First Name and Email system questions must be mapped, all others are optional. You can also leave columns unmapped if you like.', 'event_espresso'); ?>
+    <?php
+        esc_html_e(
+            // @codingStandardsIgnoreStart
+            'For Checkbox and Multi-select questions, use the pipe character ("|") to separate each answer. Eg "jedi|resistance|wookie".',
+            // @codingStandardsIgnoreEnd
+            'event_espresso'
+        );
+    ?>
 </p>
